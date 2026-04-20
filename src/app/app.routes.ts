0b22c474f46a9e054e4c17f66e472ec5658f89a2
MatteoGuala <matteoguala856@gmail.com>
2026-04-20 16:47:48 +0200

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home-component/home-component';
+import { ErrorComponent } from './components/error-component/error-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component:HomeComponent},
+    {path: '**',component:ErrorComponent}//sempre alla fine
+];
